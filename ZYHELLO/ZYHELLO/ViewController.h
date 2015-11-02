858c0fb4fa47eceb42eed8c0aca357e264d3bd60
loveMyAp <1069560719@qq.com>
2015-11-02 22:46:20 +0800

    kkkk

---
+++ ZYHELLO/ZYHELLO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZYHELLO
+//
+//  Created by mac on 15/11/2.
+//  Copyright (c) 2015年 cn.1069560719@qq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZYHELLO/ZYHELLO/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  ZYHELLO
+//
+//  Created by mac on 15/11/2.
+//  Copyright (c) 2015年 cn.1069560719@qq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"你还好吗?");
+}
+
+@end
